1f5657f433eb7b649d17af4ddeec3937d0bad0ef
Faso-main <faso.312@yandex.ru>
2025-04-17 11:47:21 +0400

    add basic num array list comprehension example | 17_4_25 11:47

---
+++ Py/list_comprehension.py
@@ -0,0 +1,5 @@
+
+array_num_original=[1,2,3,4,5] # original number array [1 to 5]
+array_num_lc=[itr for itr in range(1,6,1)] # ls nu,ber array [1 to 5]
+
+if array_num_lc == array_num_original: print(True) # array validation 
