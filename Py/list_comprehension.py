9a2c3ce27d07147d111699f1a30955e624d7a81b
Faso-main <faso.312@yandex.ru>
2025-04-17 23:15:47 +0400

    updated with integrated rules | 17_4_25 23:15

--- Py/list_comprehension.py
+++ Py/list_comprehension.py
@@ -3,3 +3,7 @@ array_num_original=[1,2,3,4,5] # original number array [1 to 5]
 array_num_lc=[itr for itr in range(1,6,1)] # ls nu,ber array [1 to 5]
 
 if array_num_lc == array_num_original: print(True) # array validation 
+
+array_num_lc_rules=[itr for itr in range(1,6,1) if itr %2] # [1 to 5 if itr %2]
+
+print(f'Updated with rules: {array_num_lc_rules}')
