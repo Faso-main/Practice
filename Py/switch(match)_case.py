fd41ac2445daef46bf7242658b2d0d7a939c3e10
Faso-main <faso.312@yandex.ru>
2025-04-28 10:54:06 +0400

    add switch case py3.10+ snippet | 28_4_25 10:53

---
+++ Py/switch(match)_case.py
@@ -0,0 +1,15 @@
+# Python 3.10+ (works like switch in c++\java)
+def number_to_string(argument):
+    match argument:
+        case 0:
+            return "zero"
+        case 1:
+            return "one"
+        case 2:
+            return "two"
+        case default:
+            return "something"
+ 
+
+head = number_to_string(2)
+print(head)
