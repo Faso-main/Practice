edc4372ebcffb59a641b9bc653d6fd7820edf774
Faso-main <faso.312@yandex.ru>
2025-05-06 11:14:25 +0400

    expend with generative examples | 6_5_25 11:10

--- Py/dictionary_comprehension.py
+++ Py/dictionary_comprehension.py
@@ -6,12 +6,15 @@ integers_array=[itr for itr in range(1,6,1)]
 str_array=[str(itr) for itr in range(1,6,1)]
 #print(integers_array,str_array)
 
-hashmap_dict={k:v for (k,v) in zip(integers_array,str_array)}
-#print(hashmap_dict)
+hashmap_dict_snippet1={k:v for (k,v) in zip(integers_array,str_array)}
+#print(hashmap_dict_snippet1)
 
-hashmap_dict=dict(zip(integers_array,str_array))
-#print(hashmap_dict)
+hashmap_dict_snippet2=dict(zip(integers_array,str_array))
+#print(hashmap_dict_snippet2)
 
 made_by_comprehension_dict={itr: itr**3 for itr in[1,2,3,4,5]}
-print(f'Made_by_comprehension_dict: {made_by_comprehension_dict}')
+#print(f'Made_by_comprehension_dict: {made_by_comprehension_dict}')
+
+conditional_statements_dict={itr:str(itr) for itr in range(1,6,1) if itr**3 % 4 == 0}
+print(f'conditional_statements_dict: {conditional_statements_dict}')
 
