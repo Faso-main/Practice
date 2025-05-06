7c2c6903f5957732293b05ee16609181a1ef5f00
Faso-main <faso.312@yandex.ru>
2025-05-06 11:55:21 +0400

    add nested_dict snippet | 6_5_25 11:55

--- Py/dictionary_comprehension.py
+++ Py/dictionary_comprehension.py
@@ -6,15 +6,20 @@ integers_array=[itr for itr in range(1,6,1)]
 str_array=[str(itr) for itr in range(1,6,1)]
 #print(integers_array,str_array)
 
-hashmap_dict_snippet1={k:v for (k,v) in zip(integers_array,str_array)}
-#print(hashmap_dict_snippet1)
+dict_snippet1={k:v for (k,v) in zip(integers_array,str_array)}
+#print(dict_snippet1)
 
-hashmap_dict_snippet2=dict(zip(integers_array,str_array))
-#print(hashmap_dict_snippet2)
+dict_snippet2=dict(zip(integers_array,str_array))
+#print(dict_snippet2)
 
 made_by_comprehension_dict={itr: itr**3 for itr in[1,2,3,4,5]}
 #print(f'Made_by_comprehension_dict: {made_by_comprehension_dict}')
 
 conditional_statements_dict={itr:str(itr) for itr in range(1,6,1) if itr**3 % 4 == 0}
-print(f'conditional_statements_dict: {conditional_statements_dict}')
+#print(f'conditional_statements_dict: {conditional_statements_dict}')
 
+hashmap_str='str1'
+nested_dict_snippet={
+    itr:{x: itr+x for x in hashmap_str} for itr in hashmap_str
+}
+print(f'nested_dict_snippet: {nested_dict_snippet}')
