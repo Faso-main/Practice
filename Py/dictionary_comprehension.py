b0c9f283aa19dcae8f1b62dc4662f590d4bda857
George <faso.312@yandex.ru>
2025-05-09 12:31:45 +0400

    update simpler logic | 9_6_25 12:31

--- Py/dictionary_comprehension.py
+++ Py/dictionary_comprehension.py
@@ -18,8 +18,8 @@ made_by_comprehension_dict={itr: itr**3 for itr in[1,2,3,4,5]}
 conditional_statements_dict={itr:str(itr) for itr in range(1,6,1) if itr**3 % 4 == 0}
 #print(f'conditional_statements_dict: {conditional_statements_dict}')
 
-hashmap_str='str1'
+hashmap='str1'
 nested_dict_snippet={
-    itr:{x: itr+x for x in hashmap_str} for itr in hashmap_str
+    itr:{x: itr+x for x in hashmap} for itr in hashmap
 }
 print(f'nested_dict_snippet: {nested_dict_snippet}')
