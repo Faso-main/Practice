88ab5e711a17f2f2394cc4f1901162cee5699041
Faso-main <faso.312@yandex.ru>
2025-05-21 10:16:34 +0400

    add test actions snippet | 21_5_25 10:16

--- Main.py
+++ Main.py
@@ -2,5 +2,8 @@
 " Welcome to the world of misery and anguish - Python practice dir!! " 
 """
 
-hashmap_str = (lambda parts: (parts[0][:6], parts[1].strip('!')))(['Python!!!!Python!!!!', 'Python!!!!'])
-print(f'Result: {" is ".join(map(str, hashmap_str))}')
+def python_forever_maker():
+    # Uses ASCII values directly within f-string and string methods
+    return f"{''.join(map(chr, [80,121,116,104,111,110]))} {chr(105)+chr(115)} {''.join(map(chr, [102,111,114,101,118,101,114]))}, {''.join(map(chr, [115,111,108,105,100,105,116,121]))} {chr(105)+chr(115)} {'better'} {'btw'}"
+
+print(python_forever_maker())
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from Main import python_forever_maker
+
+def test_python_forever_v4():
+    assert python_forever_maker() == "Python is forever, solidity is better btw"
