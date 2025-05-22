79efcbf9c3ff12d96ec421e65b19f18e3b60ce73
Faso-main <faso.312@yandex.ru>
2025-05-22 13:49:50 +0400

    remove excess print lines | 22_5_25 13:49

--- Main.py
+++ Main.py
@@ -6,4 +6,3 @@ def python_forever_maker():
     # Uses ASCII values directly within f-string and string methods
     return f"{''.join(map(chr, [80,121,116,104,111,110]))} {chr(105)+chr(115)} {''.join(map(chr, [102,111,114,101,118,101,114]))}, {''.join(map(chr, [115,111,108,105,100,105,116,121]))} {chr(105)+chr(115)} {'better'} {'btw'}"
 
-print(python_forever_maker())
