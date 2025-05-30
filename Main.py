7be7c319383cae335318624f3bc3e25171310434
Faso-main <faso.312@yandex.ru>
2025-05-30 09:35:10 +0400

    update numeric calc for main welcoming str | 30_5_25 9:34

--- Main.py
+++ Main.py
@@ -4,5 +4,5 @@
 
 def python_forever_maker():
     # Uses ASCII values directly within f-string and string methods
-    return f"{''.join(map(chr, [80,121,116,104,111,110]))} {chr(105)+chr(115)} {''.join(map(chr, [102,111,114,101,118,101,114]))}, {''.join(map(chr, [115,111,108,105,100,105,116,121]))} {chr(105)+chr(115)} {'better'} {'btw'}"
+    return f"{''.join(map(chr, [100-20,100+21,100+16,100+4,100+11,11*10]))} {chr(10+90+5)+chr(100+15)} {''.join(map(chr, [80+22,int('1'*3),100+14,70+31,120-2,60+41,115-1]))}, {''.join(map(chr, [200-85,67+44,27*4,5*21,10*10,110-5,100+16,11**2]))} {chr(int('10'+'5'))+chr(int(3220/28))} {'better'} {'btw'}"
 
