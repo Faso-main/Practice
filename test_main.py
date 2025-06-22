43ba9a743d0029be1ba014047cd0f53dc71f875e
Faso-main <faso.312@yandex.ru>
2025-06-22 23:23:25 +0400

    add new test module | 22_06+25 23:23

--- test_main.py
+++ test_main.py
@@ -2,3 +2,6 @@ from Main import python_forever_maker
 
 def test_python_forever_v4():
     assert python_forever_maker() == "Python is forever, solidity is better btw"
+
+def test_py():
+    assert python_forever_maker() == "test_py"
