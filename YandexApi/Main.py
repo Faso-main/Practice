8b36bde298b0253763a89a3d804c97901eb3b429
Faso-main <faso.312@yandex.ru>
2025-07-16 11:12:52 +0400

    add memory gen test snippet | 16_7_25 11:12

---
+++ YandexApi/Main.py
@@ -0,0 +1,5 @@
+import random
+
+def generate_combinations(itr): return [{random.randint(0, 2):random.randint(0, 2)} for items in range(1,int(itr)+1,1)]
+
+for itr in generate_combinations(5): print(f'Комбинация: {itr}')
