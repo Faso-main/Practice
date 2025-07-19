8ccfd2bb4af9c64e97f10b6caa271eb5d6976dba
George <faso.312@yandex.ru>
2025-07-19 16:15:47 +0400

    add generic fixes | 19_7_25 16:15

--- YandexApi/Main.py
+++ YandexApi/Main.py
@@ -1,5 +1,1 @@
-import random
-
-def generate_combinations(itr): return [{random.randint(0, 2):random.randint(0, 2)} for items in range(1,int(itr)+1,1)]
-
-for itr in generate_combinations(6): print(f'Комбинация: {itr}')
+print(f'I hate this company')
