b9e15d0ec3383adc4f38491172f0dba6d14c2561
Faso-main <faso.312@yandex.ru>
2025-09-04 11:46:10 +0400

    add delirious code snippet | 4_9_25 11:44

---
+++ delete_me.py
@@ -0,0 +1,5 @@
+# kinda just wanna code a little, maybe a lot 
+
+hashmap='kinda just wanna code a little, maybe a lot'
+
+for itr in range(1,len(hashmap),1): print(f'Result: well, dont really know were to satrt, so {len(hashmap[0:5])*11**2}')
